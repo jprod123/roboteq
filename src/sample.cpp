bd2a4d9b02388aa9aa65731f87c045c4aee2d528
z0sani <zacharysanichar@gmail.com>
2018-04-27 13:19:55 -0400

    creating nodehandler and subscriber

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
+#include <sstream>
+
+#include <ros/ros.h>
+
 #include <stdio.h>
-#include <string.h>
+#include <std_msgs/String.h>
 
 #include <RoboteqDevice/RoboteqDevice.h>
 #include <RoboteqDevice/ErrorCodes.h>
 #include <RoboteqDevice/Constants.h>
+#include <sensor_msgs/Joy.h>
+
+#include <std_msgs>
 
 using namespace std;
 
+
+void roboteq_cb(sensor_msgs::Joy::ConstPtr& msg)
+{
+  int left = (msg.axes[0] * 1000);
+  int right = (msg.axes[1] * 1000);
+
+	device.SetCommand(_GO, 1 ,left);
+	device.SetCommand(_GO, 2 , right);
+}
+
+
+
 int main(int argc, char *argv[])
 {
+//	
+ 	ros::init(argc, argv, "talker");
+	ros::NodeHandle z;
+	ros::Subscriber sub = z.subscriber("roboteq_cb", 1000, pub;		 
+	ros::spin();
+
+	ros::Publisher	joyPublisher publisher = n.advertise(std_msgs::String>("publisher", 1000);
+
+	
+
+//
 	string response = "";
 	RoboteqDevice device;
-	int status = device.Connect("/dev/ttyS0");
+	int status = device.Connect("/dev/ttyACM0", 115200);
 
 	if(status != RQ_SUCCESS)
 	{
@@ -20,35 +50,9 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	cout<<"- SetConfig(_DINA, 1, 1)...";
-	if((status = device.SetConfig(_DINA, 1, 1)) != RQ_SUCCESS)
-		cout<<"failed --> "<<status<<endl;
-	else
-		cout<<"succeeded."<<endl;
-
-	//Wait 10 ms before sending another command to device
-	sleepms(10);
-
-	int result;
-	cout<<"- GetConfig(_DINA, 1)...";
-	if((status = device.GetConfig(_DINA, 1, result)) != RQ_SUCCESS)
-		cout<<"failed --> "<<status<<endl;
-	else
-		cout<<"returned --> "<<result<<endl;
-
-	//Wait 10 ms before sending another command to device
-	sleepms(10);
-
-	cout<<"- GetValue(_ANAIN, 1)...";
-	if((status = device.GetValue(_ANAIN, 1, result)) != RQ_SUCCESS)
-		cout<<"failed --> "<<status<<endl;
-	else
-		cout<<"returned --> "<<result<<endl;
-
-	//Wait 10 ms before sending another command to device
-	sleepms(10);
-
+	
 	cout<<"- SetCommand(_GO, 1, 1)...";
+
 	if((status = device.SetCommand(_GO, 1, 1)) != RQ_SUCCESS)
 		cout<<"failed --> "<<status<<endl;
 	else
